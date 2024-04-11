596cb5c94f1d4c58dcd55b74296b269fef388008
sinan kodur <sinankodur24@gmail.com>
2024-04-11 14:41:23 +0530

    a tag removed

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Netflix from './images/Logonetflix.png';
 import Avatar from './images/avatar.png'
 import './NavBar.css'
@@ -17,10 +17,10 @@ function NavBar() {
   return (
     <div className={color ? 'navbar navbar-bg' : 'navbar'}>
       <img src={Netflix} className='logo' alt="logo" />
-      <a href='#' className='link' style={{left: '14rem'}}>Home</a>
-      <a href='#' className='link' style={{left: '19rem'}}>TV Shows</a>
-      <a href='#' className='link' style={{left: '26rem'}}>Movies</a>
-      <a href='#' className='link' style={{left: '32rem'}}>My List</a>
+      <span className='link' style={{left: '14rem'}}>Home</span>
+      <span className='link' style={{left: '19rem'}}>TV Shows</span>
+      <span className='link' style={{left: '26rem'}}>Movies</span>
+      <span className='link' style={{left: '32rem'}}>My List</span>
       <img src={Avatar} className='avatar' alt="avatar" />
     </div>
   ) 
